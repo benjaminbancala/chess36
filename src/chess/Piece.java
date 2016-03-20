5cd050af69f1e0d9afe57b51e25b37d69c56ab7e
shafanator <shafanator@gmail.com>
2016-03-20 12:40:50 -0500

    abstract piece

---
+++ src/chess/Piece.java
@@ -0,0 +1,19 @@
+package chess;
+
+public abstract class Piece {
+	private boolean moved = false;
+	private String name;
+	public String getName() {
+		return name;
+	}
+	public void setName(String in) {
+		name = in;
+	}
+	public boolean getMoved(){
+		return moved;
+	}
+	public void move(){
+		moved = true;
+	}
+
+}
