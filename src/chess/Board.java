b9a3b0ff0aa3928c012046fcce91531fd0276545
Benjamin Bancala <benb0227@aim.com>
2016-03-20 15:19:30 -0400

    Merge branch 'master' of https://bitbucket.org/mss302/chess36
    # Conflicts:
    #	src/chess/Chess.java

--- src/chess/Board.java
+++ src/chess/Board.java
@@ -5,5 +5,37 @@ package chess;
  *
  */
 public class Board {
+	private Piece[][] theBoard;
+	public Board(){
+		theBoard = new Piece[8][8];
+		for(int i = 0; i<theBoard.length; i++){
+			for(int j = 0; j<theBoard[i].length; j++){
+				theBoard[i][j] = null;
+			}
+		}
+	}
+	public String toString(){
+		String output = "";
+		for(int i = theBoard.length-1; i>=0; i--){
+			for(int j = 0; j<theBoard[i].length; j++){
+				if(theBoard[i][j] != null)
+					output += theBoard[i][j];
+				else{
+					if((i%2 == 0 && j%2 == 0) || (i%2 == 1 && j%2 == 1))
+						output += "## ";
+					else
+						output += "   ";
+				}
+					
+			}
+			output += i+1;
+			output += "\n";
+		}
+		output += " a  b  c  d  e  f  g  h";
+		return output;
+	}
+	public boolean getNoCheckmate(){
+		return true;
+	}
 
 }
--- src/chess/Chess.java
+++ src/chess/Chess.java
@@ -1,14 +1,45 @@
 package chess;
+<<<<<<< HEAD
 /**
  * 
  * @author Ben Bancala and Michael Shafran
  *
  */
+=======
+
+import java.util.Scanner;
+
+>>>>>>> 4dfc8301f2f9e110f28f39435357e795de954e44
 public class Chess {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
+		Board b = new Board();
+		Scanner in = new Scanner(System.in);
+		String move;
+		boolean whiteturn = true;
+		boolean nocheckmate = true;
+		
+		move = in.next();
+		System.out.println(formatMove(move));
+		
+//		while(nocheckmate){
+//			System.out.println(b);
+//			if(whiteturn)
+//				System.out.println("White's move:");
+//			else
+//				System.out.println("Black's move:");
+//			move = in.next();
+//			
+//			nocheckmate = b.getNoCheckmate();
+//			
+//		}
 
+	}
+	public static boolean formatMove(String move){
+		String moveString = "([a-h][1-8])(\\b)";
+		return move.matches(moveString);
+		
 	}
 
 }
